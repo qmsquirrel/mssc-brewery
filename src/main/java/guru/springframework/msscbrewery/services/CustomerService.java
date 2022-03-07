de221521134d93447d217eb98a38dd64f6db4c5d
Ralph L <lettau.ralph@gmail.com>
2022-03-07 16:46:57 +0100

    Customer CRUD

--- src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
@@ -6,4 +6,10 @@ import java.util.UUID;
 
 public interface CustomerService {
     CustomerDTO getCustomerById(UUID customerID);
+
+    CustomerDTO saveNewCustomer(CustomerDTO customerDTO);
+
+    void updateCustomer(UUID customerId, CustomerDTO customerDTO);
+
+    void deleteById(UUID customerId);
 }
--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,14 +1,32 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.CustomerDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerDTO getCustomerById(UUID customerID) {
         return  CustomerDTO.builder().name("John Dee").id(UUID.randomUUID()).build();
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customerDTO) {
+        return CustomerDTO.builder()
+                .id(UUID.randomUUID()).build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDTO customerDTO) {
+        // todo impls
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting a beer");
     }
 }
